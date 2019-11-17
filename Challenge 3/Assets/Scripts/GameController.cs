d70aec4ce56676c61a61b78075c5bee1f4c93e25
pandro35 <55207319+pandro35@users.noreply.github.com>
2019-11-17 17:28:03 -0500

    Challenge 3

---
+++ Challenge 3/Assets/Scripts/GameController.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public Text ScoreText;
+    public Text RestartText;
+    public Text GameOverText;
+    public Text winText;
+    private int score;
+
+    private bool gameOver;
+    private bool restart;
+
+    void Start()
+    {
+        gameOver = false;
+        restart = false;
+        RestartText.text = "";
+        GameOverText.text = "";
+        winText.text = "";
+        score = 0;
+        UpdateScore();
+        StartCoroutine (SpawnWaves());
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene("Main");
+            }
+        }
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                RestartText.text = "Press 'Space' for Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        ScoreText.text = "Points: " + score;
+        if (score >= 100)
+        {
+            winText.text = "You win!";
+            gameOver = true;
+            restart = true;
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        if (score >= 100)
+        {
+            GameOverText.text = "Game Created by Alejandro Porras";
+        }
+        else
+        {
+            GameOverText.text = "Game Over! Created by Alejandro Porras";
+        }
+        gameOver = true;
+    }
+}
